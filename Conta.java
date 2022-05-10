27004ab47246e973b76b73c37958f18a91c5fc7e
Elyssana Oliveira <53406214+Elyssana@users.noreply.github.com>
2022-05-09 22:34:59 -0300

    Update Conta.java

--- Conta.java
+++ Conta.java
@@ -6,7 +6,7 @@ public class Conta {
     private UUID id;
     private boolean status;
     private String name;
-    private String login;
+    private String usuario;
     private String senha;
     private ArrayList<Comunidade> comunidades;
     private ArrayList<Conta> amigos;
@@ -14,13 +14,11 @@ public class Conta {
     private ArrayList<Atributo> atributos;
     private ArrayList<MensagemDireta> mensagensDiretas;
 
-    
-
-    public Conta(UUID id, boolean status, String name, String login, String senha) {
+    public Conta(UUID id, String name, String usuario, String senha) {
         this.id = id;
-        this.status = status;
+        this.status = true;
         this.name = name;
-        this.login = login;
+        this.usuario = usuario;
         this.senha = senha;
         this.comunidades = new ArrayList<Comunidade>();
         this.amigos = new ArrayList<Conta>();
@@ -31,97 +29,113 @@ public class Conta {
     }
 
     public void editarPerfil() {
-        do{
-        System.out.println("\nQue alteração deseja realizar no seu perfil?\n");
-        System.out.println("1-alterar nome");
-        System.out.println("2-adicionar atributo");
-        System.out.println("3-editar atributo");
-        System.out.print("4-deletar atributo\n>>");
+        do {
+            System.out.println("\nQue alteração deseja realizar no seu perfil?\n");
+            System.out.println("1-alterar nome");
+            System.out.println("2-adicionar atributo");
+            System.out.println("3-editar atributo");
+            System.out.print("4-deletar atributo\n>>");
 
-        Scanner in = new Scanner(System.in);
+            Scanner in = new Scanner(System.in);
 
-        int opcao = in.nextInt();
-        String novoNome, novoValor;
-        switch (opcao) {
-            case 1:
+            int opcao = in.nextInt();
+            String novoNome, novoValor;
+            switch (opcao) {
+                case 1:
 
-                System.out.print("\nDigite o novo nome:\n>>");
+                    System.out.print("\nDigite o novo nome:\n>>");
 
-                in.nextLine();
-                novoNome = in.nextLine();
+                    in.nextLine();
+                    novoNome = in.nextLine();
 
-                setName(novoNome);
-                System.out.println(getName());
+                    setName(novoNome);
+                    System.out.println(getName());
 
-                break;
+                    break;
 
-            case 2:
+                case 2:
 
-                System.out.print("\nDê um nome ao novo atributo:\n>>");
-                in.nextLine();
-                novoNome = in.nextLine();
+                    System.out.print("\nDê um nome ao novo atributo:\n>>");
+                    in.nextLine();
+                    novoNome = in.nextLine();
 
-                System.out.print("\nQual valor deseja adicionar à " + novoNome + "\n>>");
-                novoValor = in.nextLine();
+                    System.out.print("\nQual valor deseja adicionar à " + novoNome + "\n>>");
+                    novoValor = in.nextLine();
 
-                Atributo novoAtributo = new Atributo(UUID.randomUUID(), novoNome, novoValor);
-                atributos.add(novoAtributo);
+                    Atributo novoAtributo = new Atributo(UUID.randomUUID(), novoNome, novoValor);
+                    atributos.add(novoAtributo);
 
-                /* Atributo atributo = atributos.get(0);
-                System.out.println("nome: " + atributo.getName());
-                System.out.println("valor: " + atributo.getValor());
-                System.out.println("id: " + atributo.getId()); */
+                    /*
+                     * Atributo atributo = atributos.get(0);
+                     * System.out.println("nome: " + atributo.getName());
+                     * System.out.println("valor: " + atributo.getValor());
+                     * System.out.println("id: " + atributo.getId());
+                     */
 
-                break;
+                    break;
 
-            case 3:
+                case 3:
 
-                System.out.println("Insira o índice do atributo à ser editado:");
-                
-                listarAtributos();
-                System.out.print(">>");
+                    System.out.println("\nInsira o índice do atributo à ser editado:");
 
-                int optAtb = in.nextInt();
+                    listarAtributos();
+                    System.out.print(">>");
 
-                System.out.print("\nInsira o novo valor de "+ (atributos.get(optAtb)).getName()+"\n>>");
-                in.nextLine();
-                novoValor = in.nextLine();
+                    int optAtb = in.nextInt();
 
-                (atributos.get(optAtb)).setValor(novoValor);
+                    System.out.print("\nInsira o novo valor de " + (atributos.get(optAtb)).getName() + "\n>>");
+                    in.nextLine();
+                    novoValor = in.nextLine();
 
-                /* for (Atributo atb : atributos) {
-                    System.out.print(cont+" - ");
-                    System.out.println(atb.toString());
-                    ++cont;
-                } */
+                    (atributos.get(optAtb)).setValor(novoValor);
 
-                break;
-            case 4:
+                    /*
+                     * for (Atributo atb : atributos) {
+                     * System.out.print(cont+" - ");
+                     * System.out.println(atb.toString());
+                     * ++cont;
+                     * }
+                     */
 
-                System.out.println("\nInsira o índice do atributo à ser deletado:");
+                    break;
+                case 4:
 
-                listarAtributos();
-                
-                System.out.print(">>");
-                
-                optAtb = in.nextInt();
+                    System.out.println("\nInsira o índice do atributo à ser deletado:");
 
-                atributos.remove(optAtb);
-                
-                break;
+                    listarAtributos();
 
-            default:
-                break;
-        }
-        }while(true);
+                    System.out.print(">>");
+
+                    optAtb = in.nextInt();
+
+                    atributos.remove(optAtb);
+
+                    break;
+
+                default:
+                    break;
+            }
+        } while (true);
         /* -alterar nome */
 
     }
 
-    public void listarAtributos(){
+    public void adicionarAmigo(IfaceCRUD CRUD){
+        Scanner in = new Scanner(System.in);
+
+        //encontrar na lista  de usuarios o usuarios que eu quero
+        System.out.print("Insira o nome de usuario da pessoa:\n>");
+        String usuario = in.nextLine();
+        
+         CRUD.getConta(usuario);
+        //adicionar o convite na lista de convites dela
+        //se ela tiver enviado um convit pra mim tbm ambos saõ adicionados a lista de amigos um do outro
+    }
+
+    public void listarAtributos() {
         int cont = 0;
         for (Atributo atb : atributos) {
-            System.out.print(cont+" - ");
+            System.out.print(cont + " - ");
             System.out.println(atb.toString());
             ++cont;
         }
@@ -151,12 +165,12 @@ public class Conta {
         this.name = name;
     }
 
-    public String getLogin() {
-        return login;
+    public String getUsuario() {
+        return usuario;
     }
 
-    public void setLogin(String login) {
-        this.login = login;
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
     }
 
     public String getSenha() {
@@ -207,6 +221,16 @@ public class Conta {
 
     public void setMensagensDiretas(ArrayList<MensagemDireta> mensagensDiretas) {
         this.mensagensDiretas = mensagensDiretas;
+    }
+
+    @Override
+    public String toString() {
+        return "****************CONTA***************** \n\nId: "
+                + id
+                + "\nNome: " + name
+                + "\nUsuario: " + usuario
+                + "\nSenha: " + senha
+                + "\n***************************************";
     }
 
 }
