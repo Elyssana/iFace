118a4c3aa3a9829e596a2c39cddc67506f4830fd
Elyssana Oliveira <53406214+Elyssana@users.noreply.github.com>
2022-05-09 22:35:05 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,23 @@
 import java.util.UUID;
 
 public class Main {
-    public static void main(String[] args){
-        Conta novaConta = new Conta(UUID.randomUUID(), 
-        true, 
-        "Elyssana", 
-        "Ely", 
-        "1234");
-        
-        novaConta.editarPerfil();
+    public static void main(String[] args) {
+        IfaceCRUD CRUD = new IfaceCRUD();
+
+        CRUD.criarConta("Elyssana",
+                "Ely",
+                "1234");
+
+        CRUD.criarConta("joao",
+                "John",
+                "4321");
+        CRUD.listarContas();
+
+        Conta userLog = CRUD.login();
+
+        //TODO criar menu
+
+        userLog.adicionarAmigo(CRUD);
     }
 
 
